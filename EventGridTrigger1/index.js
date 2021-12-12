5aca7affc5ffc6c0cf904f7a15abce28b5dfa184
Michael Nestler <michael.nestler@fh-joanneum.at>
2021-12-12 05:51:38 +0100

    initial commit

---
+++ EventGridTrigger1/index.js
@@ -0,0 +1,69 @@
+const stream = require('stream');
+const Jimp = require('jimp');
+
+const {
+    Aborter,
+    BlobURL,
+    BlockBlobURL,
+    ContainerURL,
+    ServiceURL,
+    SharedKeyCredential,
+    StorageURL,
+    uploadStreamToBlockBlob
+} = require("@azure/storage-blob");
+
+const ONE_MEGABYTE = 1024 * 1024;
+const ONE_MINUTE = 60 * 1000;
+const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
+
+const containerName = process.env.BLOB_CONTAINER_NAME;
+const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const accessKey = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+
+const sharedKeyCredential = new SharedKeyCredential(
+    accountName,
+    accessKey);
+const pipeline = StorageURL.newPipeline(sharedKeyCredential);
+const serviceURL = new ServiceURL(
+    `https://${accountName}.blob.core.windows.net`,
+    pipeline
+);
+
+module.exports = async function (context, eventGridEvent, inputBlob) {
+    context.log("Hello World");
+    context.log(typeof eventGridEvent);
+    context.log(eventGridEvent);
+    context.log(context);
+
+    const aborter = Aborter.timeout(30 * ONE_MINUTE);
+    const widthInPixels = 100;
+    const contentType = context.bindingData.data.contentType;
+
+    const blobUrl = context.bindingData.data.url;
+    context.log(blobUrl);
+    const blobName = blobUrl.slice(blobUrl.lastIndexOf("/") + 1);
+    context.log(blobName);
+    const image = await Jimp.read(inputBlob);
+    const thumbnail = image.resize(widthInPixels, Jimp.AUTO);
+    const thumbnailBuffer = await thumbnail.getBufferAsync(Jimp.AUTO);
+    const readStream = stream.PassThrough();
+    readStream.end(thumbnailBuffer);
+
+    const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+    const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
+    try {
+
+        await uploadStreamToBlockBlob(aborter, readStream,
+            blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers,
+            { blobHTTPHeaders: { blobContentType: "image/*" } });
+
+    } catch (err) {
+
+        context.log(err.message);
+
+    } finally {
+
+        context.done();
+
+    }
+};
